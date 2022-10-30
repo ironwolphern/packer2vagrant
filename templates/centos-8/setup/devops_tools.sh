989aa12386efc2db5dbc8c8c9750098fed5ceb04
ironwolphern <ironwolphern@outlook.com>
2022-10-30 23:51:48 +0100

    Add script install devops tools

---
+++ templates/centos-8/setup/devops_tools.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -eux
+
+# Install repos for devops tools
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+dnf install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
+tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
+# Install devops tools
+dnf install -y \
+    ansible \
+    terraform \
+    packer \
+    awscli \
+    azure-cli \
+    google-cloud-cli \
+    bash-completion \
+    htop \
+    jq \
+
